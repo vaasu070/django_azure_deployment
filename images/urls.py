552d1b22c956168ef0d2ec5ac039c9d3dbeb6b80
srinivasst@arkaenergy.com <srinivasst@arkaenergy.com>
2024-04-15 16:02:53 +0530

    initial commit

--- images/urls.py
+++ images/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index_view, name='images-index'),
-    path('upload/', views.upload_view, name='images-upload'),
-]
+    path('test/', views.index_view, name='images-index'),
+]   
--- images/views.py
+++ images/views.py
@@ -10,28 +10,8 @@ from images.tables import ImageTable
 
 logger = logging.getLogger(__name__)
 
-
 def index_view(request):
-    images = Image.objects.all()
-    image_table = ImageTable(images)
-    upload_form = UploadForm()
+    
+    from django.http import JsonResponse
+    return JsonResponse({'foo':'bar'})
 
-    return render(request, 'images/index.html', {
-        'images': images,
-        'image_table': image_table,
-        'upload_form': upload_form,
-    })
-
-
-@require_http_methods(["POST"])
-def upload_view(request):
-    upload_form = UploadForm(data=request.POST, files=request.FILES)
-
-    if upload_form.is_valid():
-        upload_form.save(commit=True)
-    else:
-        logger.warning("Something went wrong with uploading the file.")
-        logger.warning(request.POST)
-        logger.warning(request.FILES)
-
-    return redirect('images-index')
